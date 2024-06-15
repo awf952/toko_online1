cdc3eba60a4c2c1d7b21e5172a7d1da941a29ce0
awf952 <144993468+awf952@users.noreply.github.com>
2024-06-15 13:32:01 +0700

    first commit

--- daftar.php
+++ daftar.php
@@ -1,103 +1,88 @@
 <?php include 'header.php'; ?>
 
-<div class="row">
-  <div class="container">
-    <div class="page-header">
-      <h3>Daftar <small>Pendaftaran bidik misi</small></h3>
-    </div>
-
-    <div class="col-lg-6 col-lg-offset-3">
 
 
-
-      <?php 
-      if(isset($_GET['alert'])){
-        if($_GET['alert'] == "tidakterdaftar"){
-          echo "<div class='alert alert-danger text-center'>Maaf! anda tidak dapat mendaftar, karena NIM anda tidak terdaftar.</div>";
-        }elseif($_GET['alert'] == "foto"){
-          echo "<div class='alert alert-danger text-center'>Maaf! hanya file gambar yang boleh diupload pada form foto.</div>";
-        }elseif($_GET['alert'] == "terdaftar"){
-          echo "<div class='alert alert-success text-center'>Selamat! Anda telah berhasil terdaftar, selanjutnya silahkan <a href='login_mahasiswa.php'>login</a>.</div>";
-        }
-      }
-      ?>
-
-
-      <form method="post" action="daftar_act.php" enctype="multipart/form-data">
-        <div class="form-group">
-          <label>Nama</label>
-          <input type="text" name="nama" class="form-control" placeholder="Masukkan nama lengkap anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>NIM</label>
-          <input type="number" name="nim" class="form-control" placeholder="Masukkan NIM anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Fakultas</label>
-          <input type="text" name="fakultas" class="form-control" placeholder="Masukkan fakultas anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Jurusan</label>
-          <input type="text" name="jurusan" class="form-control" placeholder="Masukkan jurusan anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Alamat (Alamat sesuai KTP)</label>
-          <textarea class="form-control" name="alamat" placeholder="Masukkan alamat lengkap anda .." required="required"></textarea>
-        </div>
-
-        <div class="form-group">
-          <label>Alamat Sekarang</label>
-          <textarea class="form-control" name="alamat_sekarang" placeholder="Masukkan alamat lengkap anda .." required="required"></textarea>
-        </div>
-
-        <div class="form-group">
-          <label>Nomor HP/telpon</label>
-          <input type="number" name="no_telpon" class="form-control" placeholder="Masukkan nomor HP ortu anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Nomor Whatsapp</label>
-          <input type="number" name="no_wa" class="form-control" placeholder="Masukkan nomor Whatsapp anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Nomor HP/telpon Ortu</label>
-          <input type="number" name="no_ortu" class="form-control" placeholder="Masukkan nomor HP ortu anda .." required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Agama</label>
-          <select name="agama" class="form-control" required="required">
-            <option value="Islam">Islam</option>
-            <option value="Kristen">Kristen</option>
-            <option value="Hindu">Hindu</option>
-            <option value="Budha">Budha</option>
-          </select>
-        </div>
-
-        <div class="form-group">
-          <label>Foto</label>
-          <input type="file" name="foto" required="required">
-        </div>
-
-        <div class="form-group">
-          <label>Password</label>
-          <input type="text" name="password" class="form-control" placeholder="Masukkan password anda (digunakan untuk login ke sistem) .." required="required">
-        </div>
-
-        <div class="form-group">
-          <input type="submit" class="btn btn-primary" value="Ajukan Pendaftaran">
-        </div>
-
-      </form>
-
-    </div>
-  </div>
+<!-- BREADCRUMB -->
+<div id="breadcrumb">
+	<div class="container">
+		<ul class="breadcrumb">
+			<li><a href="index.php">Home</a></li>
+			<li class="active">Daftar Customer</li>
+		</ul>
+	</div>
 </div>
+<!-- /BREADCRUMB -->
+
+<!-- section -->
+<div class="section">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+			
+			<div class="col-md-12">
+				<div class="order-summary clearfix">
+					<div class="section-title">
+						<h3 class="title">Pendaftaran Customer Baru</h3>
+					</div>
+
+					<?php 
+					if(isset($_GET['alert'])){
+						if($_GET['alert'] == "duplikat"){
+							echo "<div class='alert alert-danger text-center'>Maaf email ini sudah digunakan, silahkan gunakan email yang lain.</div>";
+						}
+					}
+					?>
+
+					<div class="row">
+						<div class="col-lg-6 col-lg-offset-3">
+							
+							<form action="daftar_act.php" method="post">
+								<div class="form-group">
+									<label for="">Nama Lengkap</label>
+									<input type="text" class="input" required="required" name="nama" placeholder="Masukkan nama lengkap ..">
+								</div>
+
+								<div class="form-group">
+									<label for="">Email</label>
+									<input type="email" class="input" required="required" name="email" placeholder="Masukkan email ..">
+								</div>
+
+								<div class="form-group">
+									<label for="">Nomor HP / Whatsapp</label>
+									<input type="number" class="input" required="required" name="hp" placeholder="Masukkan nomor HP/Whatsapp ..">
+								</div>
+
+								<div class="form-group">
+									<label for="">Alamat Lengkap</label>
+									<input type="text" class="input" required="required" name="alamat" placeholder="Masukkan alamat lengkap ..">
+								</div>
+
+								<div class="form-group">
+									<label for="">Password</label>
+									<input type="password" class="input" required="required" name="password" placeholder="Masukkan password ..">
+									<small class="text-muted">Password ini digunakan untuk login ke akun anda.</small>
+								</div>
+
+								<div class="form-group">
+									<input type="submit" class="primary-btn btn-block" value="Daftar">
+								</div>
+							</form>
+
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</div>
+<!-- /section -->
+
 
 
 <?php include 'footer.php'; ?>
--- header.php
+++ header.php
@@ -1,160 +1,303 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sistem Informasi Manajemen Beasiswa Bidikmisi</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="assets/bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="assets/bower_components/Ionicons/css/ionicons.min.css">
-  <!-- DataTables -->
-  <!-- Date Picker -->
-  <link rel="stylesheet" href="assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="assets/bower_components/bootstrap-daterangepicker/daterangepicker.css">
-  
-  <link rel="stylesheet" href="assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="assets/dist/css/AdminLTE.min.css">
-  <link rel="stylesheet" href="assets/dist/css/style.css">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>TOKO ONLINE MAINAN TERBAIK</title>
 
-  <link rel="stylesheet" href="assets/dist/css/skins/_all-skins.min.css">
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
 
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="frontend/css/bootstrap.min.css" />
+
+	<!-- Slick -->
+	<link type="text/css" rel="stylesheet" href="frontend/css/slick.css" />
+	<link type="text/css" rel="stylesheet" href="frontend/css/slick-theme.css" />
+
+	<!-- nouislider -->
+	<link type="text/css" rel="stylesheet" href="frontend/css/nouislider.min.css" />
+
+	<!-- Font Awesome Icon -->
+	<link rel="stylesheet" href="frontend/css/font-awesome.min.css">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="frontend/css/style.css" />
 </head>
-<body class="hold-transition skin-red sidebar-mini">
 
-  <?php 
-  session_start();
-  include 'koneksi.php';
-  ?>
+<?php
+include 'koneksi.php';
+
+session_start();
+
+$file = basename($_SERVER['PHP_SELF']);
+
+if(!isset($_SESSION['customer_status'])){
+
+	// halaman yg dilindungi jika customer belum login
+	$lindungi = array('customer.php','customer_logout.php');
+
+	// periksa halaman, jika belum login ke halaman di atas, maka alihkan halaman
+	if(in_array($file, $lindungi)){
+		header("location:index.php");
+	}
+
+	if($file == "checkout.php"){
+		header("location:masuk.php?alert=login-dulu");
+	}
+
+}else{
+
+	// halaman yg tidak boleh diakses jika customer sudah login
+	$lindungi = array('masuk.php','daftar.php');
+
+	// periksa halaman, jika sudah dan mengakses halaman di atas, maka alihkan halaman
+	if(in_array($file, $lindungi)){
+		header("location:customer.php");
+	}
+
+}
 
 
-  <nav class="navbar navbar-default2 navbar-inverse" style="margin: 0">
-    <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php">SIMABID</a>
-      </div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Link</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <?php 
-          if(isset($_SESSION['login'])){
-            if($_SESSION['login'] == "mahasiswa"){
-              ?>
-              <li class="user user-menu">
-                <a href="mahasiswa/index.php">
-                 <?php 
-                 $id_mhs = $_SESSION['id'];
-                 $a=mysqli_query($koneksi,"SELECT * FROM mahasiswa WHERE mahasiswa_id='$id_mhs'");
-                 $aa = mysqli_fetch_assoc($a);
-                 if($aa['mahasiswa_foto']!="" && file_exists("gambar/mahasiswa/".$aa['mahasiswa_foto'])){ 
-                  ?>
-                  <img src="gambar/mahasiswa/<?php echo $aa['mahasiswa_foto']; ?>" class="user-image">
-                <?php }else{ ?>
-                  <img src="assets/dist/img/avatar5.png" class="user-image">
-                <?php } ?>
-                <?php 
-                $x = $_SESSION['id'];
-                $ad = mysqli_query($koneksi,"SELECT * FROM mahasiswa WHERE mahasiswa_id='$x'");
-                $aa = mysqli_fetch_assoc($ad);
-                ?>
-                <span class="hidden-xs">NAMA : <?php echo $aa['mahasiswa_nama']; ?> | NIM : <?php echo $aa['mahasiswa_nim']; ?></span>
-              </a>
-            </li>
-            <?php
-          }elseif($_SESSION['login'] == "pembina"){
-            ?>
-            <li class="user user-menu">
-              <a href="pembina/index.php">
-               <?php 
-               $id_mhs = $_SESSION['id'];
-               $a=mysqli_query($koneksi,"SELECT * FROM pembina WHERE pembina_id='$id_mhs'");
-               $aa = mysqli_fetch_assoc($a);
-               if($aa['pembina_foto']!="" && file_exists("gambar/pembina/".$aa['pembina_foto'])){ 
-                ?>
-                <img src="gambar/pembina/<?php echo $aa['pembina_foto']; ?>" class="user-image">
-              <?php }else{ ?>
-                <img src="assets/dist/img/avatar5.png" class="user-image">
-              <?php } ?>
-              <?php 
-              $x = $_SESSION['id'];
-              $ad = mysqli_query($koneksi,"SELECT * FROM pembina WHERE pembina_id='$x'");
-              $aa = mysqli_fetch_assoc($ad);
-              ?>
-              <span class="hidden-xs">NAMA : <?php echo $aa['pembina_nama']; ?> | PEMBINA</span>
-            </a>
-          </li>
-            <?php
-          }elseif($_SESSION['login'] == "pengurus"){
-            ?>
-            <li class="user user-menu">
-              <a href="pengurus/index.php">
-               <?php 
-               $id_mhs = $_SESSION['id'];
-               $a=mysqli_query($koneksi,"SELECT * FROM pengurus WHERE pengurus_id='$id_mhs'");
-               $aa = mysqli_fetch_assoc($a);
-               if($aa['pengurus_foto']!="" && file_exists("gambar/pengurus/".$aa['pengurus_foto'])){ 
-                ?>
-                <img src="gambar/pengurus/<?php echo $aa['pengurus_foto']; ?>" class="user-image">
-              <?php }else{ ?>
-                <img src="assets/dist/img/avatar5.png" class="user-image">
-              <?php } ?>
-              <?php 
-              $x = $_SESSION['id'];
-              $ad = mysqli_query($koneksi,"SELECT * FROM pengurus WHERE pengurus_id='$x'");
-              $aa = mysqli_fetch_assoc($ad);
-              ?>
-              <span class="hidden-xs">NAMA : <?php echo $aa['pengurus_nama']; ?> | PENGURUS</span>
-            </a>
-          </li>
-          <?php
-        }
-      }else{
-        ?>
-        <li> <a href="daftar.php"><i class="fa fa-lock"></i> &nbsp; DAFTAR</a></li>
-        <li> <a href="login_mahasiswa.php"><i class="fa fa-lock"></i> &nbsp; LOGIN MAHASISWA</a></li>
-        <li> <a href="login.php"><i class="fa fa-lock"></i> &nbsp; LOGIN</a></li>
-        <?php
-      }
-      ?>
-    </ul>
-      <!-- <form class="navbar-form navbar-right">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form> -->
-    </div>
-  </div>
-</div>
-</nav>
+if($file == "checkout.php"){
+
+
+	if(!isset($_SESSION['keranjang']) || count($_SESSION['keranjang']) == 0){
+
+		header("location:keranjang.php?alert=keranjang_kosong");
+
+	}
+}
+
+
+
+?>
+<body>
+
+	<style>
+		.product-name {
+			height: 5px;
+		}
+	</style>
+	<!-- HEADER -->
+	<header>
+		
+		<!-- header -->
+		<div id="header">
+			<div class="container">
+				<div class="pull-left">
+					<!-- Logo -->
+					<div class="header-logo">
+						<a class="logo" href="index.php">
+							<img src="frontend/img/logo.jpeg" alt="">
+						</a>
+					</div>
+					<!-- /Logo -->
+
+					<!-- Search -->
+					<div class="header-search">
+						<form action="index.php" method="get">
+							<input class="input" type="text" name="cari" placeholder="Masukkan Pencarian ..">
+							<button class="search-btn"><i class="fa fa-search"></i></button>
+						</form>
+					</div>
+					<!-- /Search -->
+				</div>
+				<div class="pull-right">
+					<ul class="header-btns">
+						
+						<!-- Cart -->
+						<li class="header-cart dropdown default-dropdown">
+
+							<?php 
+							if(isset($_SESSION['keranjang'])){
+								$jumlah_isi_keranjang = count($_SESSION['keranjang']);
+							}else{
+								$jumlah_isi_keranjang = 0;
+							}
+							?>
+
+							<a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+								<div class="header-btns-icon">
+									<i class="fa fa-shopping-cart"></i>
+									<span class="qty"><?php echo $jumlah_isi_keranjang; ?></span>
+								</div>
+								<strong class="text-uppercase">Keranjang Belanja :</strong>
+								<br>
+								<?php 
+								$total = 0;
+								if(isset($_SESSION['keranjang'])){
+									$jumlah_isi_keranjang = count($_SESSION['keranjang']);
+									for($a = 0; $a < $jumlah_isi_keranjang; $a++){
+										$id_produk = $_SESSION['keranjang'][$a]['produk'];
+										$isi = mysqli_query($koneksi,"select * from produk where produk_id='$id_produk'");
+										$i = mysqli_fetch_assoc($isi);
+										$total += $i['produk_harga'];
+									}
+								}
+								?>
+								<span><?php echo "Rp. ".number_format($total)." ,-"; ?></span>
+							</a>
+							<div class="custom-menu">
+								<div id="shopping-cart">
+									<div class="shopping-cart-list">
+										<?php 
+										$total_berat = 0;
+										if(isset($_SESSION['keranjang'])){
+
+											$jumlah_isi_keranjang = count($_SESSION['keranjang']);
+
+											if($jumlah_isi_keranjang != 0){
+												// cek apakah produk sudah ada dalam keranjang
+												for($a = 0; $a < $jumlah_isi_keranjang; $a++){
+													$id_produk = $_SESSION['keranjang'][$a]['produk'];
+													$isi = mysqli_query($koneksi,"select * from produk where produk_id='$id_produk'");
+													$i = mysqli_fetch_assoc($isi);
+
+													$total_berat += $i['produk_berat'];
+													?>
+
+													<div class="product product-widget">
+														<div class="product-thumb">
+															<?php if($i['produk_foto1'] == ""){ ?>
+																<img src="gambar/sistem/produk.png">
+															<?php }else{ ?>
+																<img src="gambar/produk/<?php echo $i['produk_foto1'] ?>">
+															<?php } ?>
+														</div>
+														<div class="product-body">
+															<h3 class="product-price"><?php echo "Rp. ".number_format($i['produk_harga']) . " ,-"; ?></h3>
+															<h2 class="product-name"><a href="produk_detail.php?id=<?php echo $i['produk_id'] ?>"><?php echo $i['produk_nama'] ?></a></h2>
+														</div>
+														<a class="cancel-btn" href="keranjang_hapus.php?id=<?php echo $i['produk_id']; ?>&redirect=keranjang"><i class="fa fa-trash"></i></a>
+													</div>
+
+													<?php
+
+												}
+											}else{
+												echo "<center>Keranjang Masih Kosong.</center>";
+											}
+											
+
+										}else{
+											echo "<center>Keranjang Masih Kosong.</center>";
+										}
+										?>
+										
+									</div>
+									<div class="shopping-cart-btns">
+										<a class="main-btn" href="keranjang.php">Keranjang</a>
+										<a class="primary-btn" href="checkout.php">Checkout <i class="fa fa-arrow-circle-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</li>
+						<!-- /Cart -->
+
+						<?php 
+						if(isset($_SESSION['customer_status'])){
+							$id_customer = $_SESSION['customer_id'];
+							$customer = mysqli_query($koneksi,"select * from customer where customer_id='$id_customer'");
+							$c = mysqli_fetch_assoc($customer);
+							?>
+							<!-- Account -->
+							<li class="header-account dropdown default-dropdown" style="min-width: 200px">
+								<div class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="true">
+									<div class="header-btns-icon">
+										<i class="fa fa-user-o"></i>
+									</div>
+									<strong class="text-uppercase"><?php echo $c['customer_nama']; ?> <i class="fa fa-caret-down"></i></strong>
+								</div>
+								<span><?php echo $c['customer_email']; ?></span>
+								<ul class="custom-menu">
+									<li><a href="customer.php"><i class="fa fa-user-o"></i> Akun Saya</a></li>
+									<li><a href="customer_pesanan.php"><i class="fa fa-list"></i> Pesanan Saya</a></li>
+									<li><a href="customer_password.php"><i class="fa fa-lock"></i> Ganti Password</a></li>
+									<li><a href="customer_logout.php"><i class="fa fa-sign-out"></i> Keluar</a></li>
+								</ul>
+							</li>
+							<!-- /Account -->
+							<?php
+						}else{
+							?>
+							<li class="header-account dropdown default-dropdown">
+								<a href="masuk.php" class="text-uppercase main-btn">Login</a> 
+								<a href="daftar.php" class="text-uppercase primary-btn">Daftar</a> 
+							</li>
+							<?php
+						}
+						?>
+
+						<!-- Mobile nav toggle-->
+						<li class="nav-toggle">
+							<button class="nav-toggle-btn main-btn icon-btn"><i class="fa fa-bars"></i></button>
+						</li>
+						<!-- / Mobile nav toggle -->
+					</ul>
+				</div>
+			</div>
+			<!-- header -->
+		</div>
+		<!-- container -->
+	</header>
+	<!-- /HEADER -->
+
+	<!-- NAVIGATION -->
+	<div id="navigation">
+		<!-- container -->
+		<div class="container">
+			<div id="responsive-nav">
+				<!-- category nav -->
+				<div class="category-nav show-on-click">
+					<span class="category-header">Kategori Produk <i class="fa fa-list"></i></span>
+					<ul class="category-list">
+						<?php 
+						$data = mysqli_query($koneksi,"SELECT * FROM kategori");
+						while($d = mysqli_fetch_array($data)){
+							?>
+							<li><a href="produk_kategori.php?id=<?php echo $d['kategori_id']; ?>"><?php echo $d['kategori_nama']; ?></a></li>
+							<?php 
+						}
+						?>
+						<li style="background: #999;"><a href="index.php" style="color: white">Tampilkan Semua</a></li>
+					</ul>
+				</div>
+				<!-- /category nav -->
+
+				<!-- menu nav -->
+				<div class="menu-nav">
+					<span class="menu-header">Menu <i class="fa fa-bars"></i></span>
+					<ul class="menu-list">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="index.php">Shop</a></li>
+						<li><a href="login.php">Login Admin</a></li>
+					</ul>
+				</div>
+				<!-- menu nav -->
+			</div>
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /NAVIGATION -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
